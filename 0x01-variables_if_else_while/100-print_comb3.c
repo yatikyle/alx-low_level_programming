ecef5692ba62e95f5a34b814fd5ee8a30d37351f
yatikyle <yatichkyle@gmail.com>
2022-07-03 10:51:14 -0700

    100-print_com3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 int number_left;
 int number_right;
 
-for (number_left = 48; number_right <=78; number_left++)
+for (number_left = 48; number_right <= 78; number_left++)
 {
 for (number_right = number_left + 1; number_right <= 78; number_right++)
 {
@@ -19,13 +19,14 @@ for (number_right = number_left + 1; number_right <= 78; number_right++)
 putchar (number_left);
 putchar (number_right);
 
-if ((number_left ==56) && (number_right == 78))
+if ((number_left == 56) && (number_right == 78))
 {
 break;
 }
 
 putchar(',');
 putchar (' ');
+
 }
 }
 
