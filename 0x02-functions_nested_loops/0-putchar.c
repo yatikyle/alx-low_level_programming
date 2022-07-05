33fbcf960b89d0d6de26d4f746b4881f8b69acd8
yatikyle <yatichkyle@gmail.com>
2022-07-05 08:17:55 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point 
+ *
+ *Return: Should return nothing, Return void
+ */
+
+int main (void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchat('r');
+_putchar('\n');
+
+return (0);
+}
