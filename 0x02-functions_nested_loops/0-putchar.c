29545945c29b2887da33d5b00003d91d1a407d92
yatikyle <yatichkyle@gmail.com>
2022-07-05 08:54:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,17 @@
  * main - Entry point
  *
  * Return: Should be 0 (Success/correct)
- */
+ **/
 
 int main(void)
 {
-	
-	char text[''] = "_putchar";
-	int i;
+char text[] = "_putchar";
+int i;
 
-	for (i = 0; i < 8; i++)
-	{
-	  _putchar(text[i]);
-	}
-
+for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
 
 _putchar ('\n');
 
