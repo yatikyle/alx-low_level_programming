5a0150c86ea39e85d8515fa96572ea4768e1af57
yatikyle <yatichkyle@gmail.com>
2022-07-05 19:50:13 -0700

    10-add.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,6 +10,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int num1, int num2);
 void print_to_98(int n);
 void print_times_table(int n);
