f55200d4ca4d145c8c32a5e490175b70d73a57d5
yatikyle <yatichkyle@gmail.com>
2022-07-05 18:43:21 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,12 @@
 /*
  *File: 6-abs.c
  *
+ *
  */
 
 /**
  * _abs - Calcute the absolute value of the integer
+ * @n: The integer to be calculated 
  *
  * Return: The absolute value of the integer.
  */
