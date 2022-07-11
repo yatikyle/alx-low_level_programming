eba7c6ac07d6092618dcd29e6ec72eedc9b1d61f
yatikyle <yatichkyle@gmail.com>
2022-07-11 12:45:43 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 /**
  *reset_to_98 - updates a integer value to 98
- *@n: a pointer to an int that can be updated 
+ *@n: a pointer to an int that will be updated
+ * 
  *Return: Always return 0 (Success/correct).
  */
 
