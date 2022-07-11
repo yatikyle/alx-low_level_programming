c2989e8c8f24d767d417058df5b7ee62c5ff995f
yatikyle <yatichkyle@gmail.com>
2022-07-11 15:03:38 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @str: The string to be encoded
+ *
+ * Return: A pointer to the encoded string
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1<)
+		{
+			printf(", ");
+		}
+
+		i++;
+	}
+		printf("\n");
+}
