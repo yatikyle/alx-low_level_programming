2e4c40fa947e96b09c4e0d66b3f8fba01f9c26dd
yatikyle <yatichkyle@gmail.com>
2022-07-11 13:36:24 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
  * print_rev - prints a string in reverse order
  *@s: A pointer to an int that will be changed 
  *
- *Return: void which means our answer is correct
+ *Return: void which means our answer is correct.
  */
 
 void print_rev(char *s)
@@ -21,5 +21,5 @@ void print_rev(char *s)
 		_putchar (s[i]);
 	}
 
-	putchar ('\n');
+	_putchar ('\n');
 }
