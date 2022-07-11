06ed571e0a205f99ba6927e00e44e8d27fa6a3ca
yatikyle <yatichkyle@gmail.com>
2022-07-11 14:52:26 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints every other character of a string
+ * @str: The string to be encoded
+ *
+ * Return: A pointer to the encoded string.
+ */
+
+void puts_half(char *str)
+{
+
+	int i, last;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	
+	last = (i + 1) / 2;
+
+	for (i = last; str[i]; i++)
+	{
+		_putchar (str[i]);
+	}
+	_putchar('\n');
+}
