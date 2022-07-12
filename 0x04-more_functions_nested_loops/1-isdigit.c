b5f8304c4e8ed2727ee1b857c2ff42c87819e4be
yatikyle <yatichkyle@gmail.com>
2022-07-12 13:45:33 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+ * _isdigit - Checks for a digiti between (0-9)
+ * @c: print int
+ *
+ * Return: 1 if the number is a digit, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
