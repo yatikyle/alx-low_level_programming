c8964a75b0877445394f0db56ab92501d6e975d1
yatikyle <yatichkyle@gmail.com>
2022-07-13 09:01:09 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+/**
+ * _putchar: writes the charater c to stdout
+ * @c: The charater to print
+ *
+ * Return: On sucess 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
