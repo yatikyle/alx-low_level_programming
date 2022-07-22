74c9b4fd38a2a275435c271e04fda9ac6f99e549
yatikyle <90194341+yatikyle@users.noreply.github.com>
2022-07-22 14:53:22 -0400

    Update 4-strpbrk.c

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be searched.
+ * @accept: The bytes being searched for.
+ *
+ * Return: If a set is matched - Pointer matches bytes.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
