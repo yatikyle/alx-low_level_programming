f2a6b2ca64d4647260705a769ffaabedb3cbed5b
yatikyle <yatichkyle@gmail.com>
2022-07-22 15:20:22 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,6 +17,6 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	else
-	printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argv - 2])));
+	printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argc - 2])));
 	return (0);
 }
