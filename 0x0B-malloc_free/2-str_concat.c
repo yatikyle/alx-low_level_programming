f46ba76571044396d7bf0862d1918f9e96ca6256
yatikyle <yatichkyle@gmail.com>
2022-07-27 12:21:06 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,13 +24,15 @@ char *str_concat(char *s1, char *s2)
 
 	l = i + j;
 	s = (char *)malloc(l * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
 	j = 0;
 
 	while (k < l)
 	{
-		if (k <= i)
+		if (k < i)
 			s[k] = s1[k];
-		if (k > i)
+		if (k >= i)
 		{
 			s[k] = s2[j];
 			j++;
