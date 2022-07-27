13da8d44663ceada9662262fe022848769bd2ac0
yatikyle <yatichkyle@gmail.com>
2022-07-27 11:50:25 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strup - Returns a pointer to a newly-allocated space in memory.
+ * @str: The string to be copied
+ *
+ * Return: A string pointer.
+ */
+char *_strdup(char *str)
+{
+	int i = 1;
+	char *s;
+	if (str == NULL)
+		return (NULL);
+	while str[i]
+		i++;
+	s = (char *)malloc(i * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+	s[j] ='\0';
+	return (s);
+}
