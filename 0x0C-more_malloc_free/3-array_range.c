58faa6948724e5e693b9f4a02659146ad5ccfc00
yatikyle <yatichkyle@gmail.com>
2022-07-28 07:22:57 -0700

    Array Range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 /**
- * array_range - Creates an array of integers from min to max.
- * @min: The first value of the array.
- * @max: The last value of the array.
+ * array_range - range printer.
+ * @min: minimum
+ * @max: maximum
  *
- * Return: If min > max or the function fails - NULL.
+ * Return: Integer array
  */
 int *array_range(int min, int max)
 {
@@ -15,16 +15,9 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-
 	size = max - min + 1;
-
-	array = malloc(sizeof(int) * size);
-
-	if (array == NULL)
-		return (NULL);
-
+	array = malloc(sizeof(int *) * size);
 	for (index = 0; index < size; index++)
 		array[index] = min++;
-
 	return (array);
 }
